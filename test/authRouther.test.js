6f2c3dd8e5cb97815fdb1fe42c720f10553c5213
Jay <sheevpa1patine1024@gmail.com>
2026-01-15 16:12:01 -0700

    Make login and logout test

---
+++ test/authRouther.test.js
@@ -0,0 +1,48 @@
+const request = require('supertest');
+const app = require('../src/service.js');
+const { Role, DB } = require('../src/database/database.js');
+
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken;
+
+beforeAll(async () => {
+  testUser.email = randomName() + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
+  expectValidJwt(testUserAuthToken);
+  testUser.token = testUserAuthToken;
+});
+
+test('login', async () => {
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+  expect(loginRes.status).toBe(200);
+  expectValidJwt(loginRes.body.token);
+
+  const expectedUser = { ...testUser, roles: [{ role: 'diner' }] };
+  delete expectedUser.password;
+  delete expectedUser.token;
+  expect(loginRes.body.user).toMatchObject(expectedUser);
+});
+
+test('logout', async () => {
+  const logoutRes = await request(app)
+    .delete('/api/auth')
+    .set("Authorization", 'Bearer ' + testUser.token)
+    .send();
+
+  expect(logoutRes.status).toBe(200);
+  
+  // const checkMe = await request(app)
+  // .get('/api/user/me')
+  // .send();
+});
+
+
+
+function expectValidJwt(potentialJwt) {
+  expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+}
+
+function randomName() {
+  return Math.random().toString(36).substring(2, 12);
+}
