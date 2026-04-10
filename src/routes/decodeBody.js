a292c829f6b71f526b9ee7b64e6d92c48ffe6ef2
erush4 <=>
2026-04-09 19:49:09 -0600

    add logging errors

--- src/routes/decodeBody.js
+++ src/routes/decodeBody.js
@@ -2,21 +2,23 @@ const {jwtVerify} = require('jose');
 const {httpJwtSecret} = require("../config");
 
 const decodeBody = async (req, res, next) => {
-    if (req.body) {
+    console.log(`decode body called on ${req.url}\n`);
+    if (req.body && Object.keys(req.body).length > 0) {
         const reqBody = req.body;
+        console.log(`body: ${JSON.stringify(reqBody)}`);
         try {
             const key = new TextEncoder().encode(httpJwtSecret);
             const {payload} = await jwtVerify(reqBody.signature, key);
-            console.log(reqBody);
             const {iat, exp, ...decodedBody} = payload;
-            console.log(decodedBody);
+            console.log(`decoded body: ${JSON.stringify(decodedBody)}\n`);
 
             if (JSON.stringify(decodedBody) !== JSON.stringify(reqBody.body)) {
                 return res.status(418).send({message: "lmao"});
             }
 
             req.body = reqBody.body;
-        } catch {
+        } catch (err) {
+            console.log(err.toString())
             return res.status(418).send({message: "lmao"});
         }
     }
